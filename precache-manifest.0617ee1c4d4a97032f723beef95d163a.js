c0b9c689ba7db49e5a32b3cd264370ce1cff1b19
AlirezaAbedi <aabedi79@gmail.com>
2020-12-08 12:45:12 +0330

    Updates

---
+++ precache-manifest.0617ee1c4d4a97032f723beef95d163a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b30bb78b8b5b3e18bc10f3f6704b9b89",
+    "url": "/BrightBee/index.html"
+  },
+  {
+    "revision": "c51d58bef79e5d83013e",
+    "url": "/BrightBee/static/css/2.f8e0039c.chunk.css"
+  },
+  {
+    "revision": "2125a428e56cac8a1719",
+    "url": "/BrightBee/static/css/main.a2aa38b4.chunk.css"
+  },
+  {
+    "revision": "c51d58bef79e5d83013e",
+    "url": "/BrightBee/static/js/2.9310cdcd.chunk.js"
+  },
+  {
+    "revision": "2406d70eebff1cb7ed36b341aea68231",
+    "url": "/BrightBee/static/js/2.9310cdcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2125a428e56cac8a1719",
+    "url": "/BrightBee/static/js/main.2537f16e.chunk.js"
+  },
+  {
+    "revision": "1499c370890409db35a5",
+    "url": "/BrightBee/static/js/runtime-main.e373e0af.js"
+  },
+  {
+    "revision": "4519aea339b63bce6e1868865586f405",
+    "url": "/BrightBee/static/media/dxicons.4519aea3.woff2"
+  },
+  {
+    "revision": "f1829bb1cb996a760a819b4eb23b6108",
+    "url": "/BrightBee/static/media/dxicons.f1829bb1.ttf"
+  },
+  {
+    "revision": "fd4164a956b105e4ec9f6c12de2d6b82",
+    "url": "/BrightBee/static/media/dxicons.fd4164a9.woff"
+  }
+]);
